82cea05723fe7f9dac8f16cc59ae9228fbe59366
Rauan de Santana Rosa <rauanoronha@hotmail.com>
2023-03-27 16:53:34 -0300

    Adicionando Suporte Windows

--- lib/importes/paginas/login/login_celular.dart
+++ lib/importes/paginas/login/login_celular.dart
@@ -12,62 +12,82 @@ class LoginCelular extends StatefulWidget {
 class _LoginCelularState extends State<LoginCelular> {
   @override
   Widget build(BuildContext context) {
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        return Componentes.pagina.padrao(
-          conteudo: <Widget>[
-            // ----------------------------------------------------------------- Escala P
-            Container(
-              width: constraints.maxWidth,
-              height: constraints.maxHeight,
-              padding: const EdgeInsets.symmetric(horizontal: 25),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  // ----------------------------------------------------------- Logo Noronha EcoTech
-                  Componentes.imagem.padrao(
-                    imagem: Estilos.imagem.logos.noronhaEcoTech.r512(context),
-                    largura: 350,
-                    altura: 250,
+    return Componentes.pagina.padrao(
+      conteudo: <Widget>[
+        // --------------------------------------------------------------------- Escala P
+        Center(
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                // ------------------------------------------------------------- Logo Noronha EcoTech
+                Componentes.imagem.padrao(
+                  imagem: Estilos.imagem.logos.noronhaEcoTech.r512(context),
+                  largura: 350,
+                  altura: 250,
+                ),
+                // ------------------------------------------------------------- Campo Celular
+                Componentes.texto.campoPadrao(
+                  textoTitulo: "Celular",
+                  tipoTeclado: TextInputType.phone,
+                  acaoBotaoTeclado: TextInputAction.next,
+                  componentePrefixo: Componentes.icone.padrao(
+                    icone: Icons.phone_android,
                   ),
+                ),
+                // ------------------------------------------------------------- Espaço
+                const Padding(padding: EdgeInsets.only(top: 10)),
+                // ------------------------------------------------------------- Campo Senha
+                Componentes.texto.campoPadrao(
+                  textoTitulo: "Senha",
+                  ocultarTexto: true,
+                  tipoTeclado: TextInputType.visiblePassword,
+                  acaoBotaoTeclado: TextInputAction.go,
+                  componentePrefixo: Componentes.icone.padrao(
+                    icone: Icons.password,
+                  ),
+                ),
+                // ------------------------------------------------------------- Espaço
+                const Padding(padding: EdgeInsets.only(top: 5)),
+                Wrap(
+                  spacing: 25,
+                  runSpacing: 10,
+                  textDirection: TextDirection.rtl,
+                  children: <Widget>[
 
-                  Wrap(
-                    spacing: 5,
-                    runSpacing: 5,
-                    textDirection: TextDirection.rtl,
-                    direction: Axis.horizontal,
-                    alignment: WrapAlignment.spaceAround,
-                    runAlignment: WrapAlignment.spaceAround,
-                    children: <Widget>[
-                      // ------------------------------------------------------- Botão Entrar
-                      Componentes.botao.elevado(
-                        aoPrecionar: () => {},
-                        titulo: "Entrar",
-                      ),
-                      // ------------------------------------------------------- Botão Cadastro
-                      Componentes.botao.borda(
-                        aoPrecionar: () => {},
-                        titulo: "Cadastrar-se",
-                      ),
-                    ],
-                  ),
-                  const Padding(padding: EdgeInsets.only(top: 40)),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      // ------------------------------------------------------- Botão Recuperar Senha
-                      Componentes.botao.texto(
-                        aoPrecionar: () => {},
-                        titulo: "Recuperar Senha",
-                      ),
-                    ],
-                  )
-                ],
-              ),
+                    // --------------------------------------------------------- Botão Entrar
+                    Componentes.botao.elevado(
+                      aoPrecionar: () => {},
+                      titulo: "Entrar",
+                    ),
+                    // --------------------------------------------------------- Botão Cadastro
+                    Componentes.botao.borda(
+                      aoPrecionar: () => {},
+                      titulo: "Cadastre-se",
+                    ),
+
+                  ],
+                ),
+                // ------------------------------------------------------------- Espaço
+                const Padding(padding: EdgeInsets.only(top: 40)),
+                // ------------------------------------------------------------- Botão Recuperar Senha
+                Componentes.botao.texto(
+                  aoPrecionar: () => {},
+                  titulo: "Recuperar Senha",
+                ),
+
+
+
+
+
+
+              ],
             ),
-          ],
-        );
-      },
+          ),
+        ),
+        // --------------------------------------------------------------------- Escala M
+      ],
     );
   }
 }
