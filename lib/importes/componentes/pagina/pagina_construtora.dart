64801ec8cbff4844fbec4c8cad6968ad7b77b83c
Rauan de Santana Rosa <rauanoronha@hotmail.com>
2023-04-17 21:36:02 -0300

    Corrigindo Componentes Pagina Construtora

--- lib/importes/componentes/pagina/pagina_construtora.dart
+++ lib/importes/componentes/pagina/pagina_construtora.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 class $ComPaginaConstrutora extends StatefulWidget {
-  final void Function(StateSetter) estadoInicial;
-  final void Function(StateSetter) estadoMontado;
-  final void Function(StateSetter) estadoDesmontado;
-  final void Function(StateSetter) estadoDescarte;
+  final void Function(StateSetter)? estadoInicial;
+  final void Function(StateSetter)? estadoMontado;
+  final void Function(StateSetter)? estadoDesmontado;
+  final void Function(StateSetter)? estadoDescarte;
   final StatefulWidgetBuilder construtor;
 
   const $ComPaginaConstrutora({
@@ -24,22 +24,26 @@ class _$ComPaginaConstrutoraState extends State<$ComPaginaConstrutora> {
   @override
   void initState() {
     super.initState();
-    widget.estadoInicial(setState);
+    (widget.estadoInicial != null) ? widget.estadoInicial!(setState) : null;
     WidgetsBinding.instance.addPostFrameCallback(
-      (_) => widget.estadoMontado(setState),
+      (_) => (widget.estadoMontado != null)
+          ? widget.estadoMontado!(setState)
+          : null,
     );
   }
 
   @override
   void deactivate() {
     super.deactivate();
-    widget.estadoDesmontado(setState);
+    (widget.estadoDesmontado != null)
+        ? widget.estadoDesmontado!(setState)
+        : null;
   }
 
   @override
   void dispose() {
     super.dispose();
-    widget.estadoDescarte(setState);
+    (widget.estadoDescarte != null) ? widget.estadoDescarte!(setState) : null;
   }
 
   @override
