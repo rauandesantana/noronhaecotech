8985a367bbe5099dc91cd0bb51d60d34d6b638ea
Rauan de Santana Rosa <rauanoronha@hotmail.com>
2023-04-30 12:32:18 -0300

    Corrigindo Estilos Texto Formatar Texto

--- lib/importes/estilos/texto/texto_formatar.dart
+++ lib/importes/estilos/texto/texto_formatar.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/services.dart';
+import 'package:noronhaecotech/importes/importar_componentes.dart';
 import 'package:noronhaecotech/importes/modelos/modelos_formatos_texto.dart';
-
-TextEditingValue valorVazio = const TextEditingValue(
-  text: "",
-  composing: TextRange.empty,
-  selection: TextSelection.collapsed(offset: 0),
-);
 
 class $EstTextoFormatar extends TextInputFormatter {
   final FormatosTexto formato;
@@ -16,59 +11,49 @@ class $EstTextoFormatar extends TextInputFormatter {
 
   @override
   TextEditingValue formatEditUpdate(oldValue, newValue) {
-    final String valorFormato = formato.valorFormato;
     final bool apagando = (oldValue.text.length > newValue.text.length);
 
     if (oldValue.text.isEmpty) {
-      String textoConvertido = formato.converter(newValue.text);
-      int indiceCursor = formato.indiceCursor(
+      final String valorFormato = formato.valorFormato;
+      final String textoDesconvertido = formato.desconverter(newValue.text);
+      final String textoConvertido = formato.converter(textoDesconvertido);
+      final int indiceCursor = formato.indiceCursor(
         textoConvertido,
         apagando,
         double.maxFinite.toInt(),
       );
+
       oldValue = oldValue.copyWith(text: valorFormato);
       newValue = newValue.copyWith(
         text: textoConvertido,
         composing: TextRange.empty,
         selection: TextSelection.collapsed(offset: indiceCursor),
       );
+
       return newValue;
     }
 
-    final int tamanhoFormato = formato.tamanhoFormato;
-    final bool finalizado =
-        formato.desconverter(oldValue.text).length == tamanhoFormato;
+    final int tamanhoTexto = formato.desconverter(oldValue.text).length;
+    final bool finalizado = tamanhoTexto == formato.tamanhoFormato;
 
     if (finalizado && !apagando) return oldValue;
 
-    return atualizar(atual: newValue, anterior: oldValue, apagando: apagando);
+    final int cursor = newValue.selection.baseOffset;
+
+    return atualizar(
+      atual: newValue,
+      anterior: oldValue,
+      cursor: cursor,
+      apagando: apagando,
+    );
   }
 
   TextEditingValue atualizar({
     required TextEditingValue atual,
     required TextEditingValue anterior,
+    required int cursor,
     required bool apagando,
   }) {
-    final int cursor = atual.selection.baseOffset;
-    final bool colando = (atual.text.length - anterior.text.length) > 1;
-
-    if (colando) {
-      final int cursorAnterior = anterior.selection.baseOffset;
-      final String texto = atual.text.substring(cursorAnterior, cursor);
-      final String textoConvertido = formato.converter(texto);
-      return atual.copyWith(
-        text: textoConvertido,
-        composing: TextRange.empty,
-        selection: TextSelection.collapsed(
-          offset: formato.indiceCursor(
-            textoConvertido,
-            apagando,
-            double.maxFinite.toInt(),
-          ),
-        ),
-      );
-    }
-
     String textoDesconvertido = formato.desconverter(atual.text);
     String textoConvertido = formato.converter(textoDesconvertido);
     int indiceCursor = formato.indiceCursor(
@@ -77,25 +62,27 @@ class $EstTextoFormatar extends TextInputFormatter {
       cursor,
     );
 
-    final bool apagandoUnitario =
-        (anterior.text.length - atual.text.length) == 1;
-    final bool semSelecao =
-        (anterior.selection.end - anterior.selection.start) == 0;
-    final bool textoValido = textoConvertido.isNotEmpty && cursor > 0;
+    if (apagando) {
+      final bool apagandoUnitario =
+          (anterior.text.length - atual.text.length) == 1;
+      final bool selecao =
+          anterior.selection.baseOffset != anterior.selection.extentOffset;
+      final bool textoValido = textoConvertido.isNotEmpty && cursor > 0;
 
-    if (apagandoUnitario && semSelecao && textoValido) {
-      String caractere = textoConvertido[cursor];
-      if (formato.checarCaractere(caractere)) {
-        String textoBusca = textoConvertido.substring(0, cursor);
-        final int indiceApagar = textoBusca.lastIndexOf(formato.regex);
-        textoBusca = textoConvertido.replaceRange(indiceApagar, cursor, "");
-        textoDesconvertido = formato.desconverter(textoBusca);
-        textoConvertido = formato.converter(textoDesconvertido);
-        indiceCursor = formato.indiceCursor(
-          textoConvertido,
-          apagando,
-          indiceApagar,
-        );
+      if (apagandoUnitario && !selecao && textoValido) {
+        final String caractere = textoConvertido[cursor];
+        if (formato.checarCaractere(caractere)) {
+          String textoBusca = textoConvertido.substring(0, cursor);
+          final int indiceApagar = textoBusca.lastIndexOf(formato.regex);
+          textoBusca = textoConvertido.replaceRange(indiceApagar, cursor, "");
+          textoDesconvertido = formato.desconverter(textoBusca);
+          textoConvertido = formato.converter(textoDesconvertido);
+          indiceCursor = formato.indiceCursor(
+            textoConvertido,
+            apagando,
+            indiceApagar,
+          );
+        }
       }
     }
 
