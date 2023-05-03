ef8145e4d5efa4390e1048fc16fccc8ba8ae85df
Rauan de Santana Rosa <rauanoronha@hotmail.com>
2023-05-03 06:45:33 -0300

    Adicionando Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:noronhaecotech/importes/importar_paginas.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Future.wait([
+    //////////////////////////////////////////////////////////////////////////// Carregamento
+    Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform),
+    ////////////////////////////////////////////////////////////////////////////
+  ]);
   runApp(const MyApp());
 }
 
@@ -15,7 +23,7 @@ class MyApp extends StatelessWidget {
       title: 'Noronha EcoTech',
       initialRoute: Paginas.rotaInicial,
       routes: Paginas.rotas,
-      //////////////////////////////////////////////////////////////////////////
+      ////////////////////////////////////////////////////////////////////////// Idiomas
       debugShowCheckedModeBanner: false,
       localizationsDelegates: const [
         Idiomas.delegate,
@@ -23,7 +31,7 @@ class MyApp extends StatelessWidget {
         GlobalCupertinoLocalizations.delegate,
       ],
       supportedLocales: Idiomas.delegate.supportedLocales,
-      //////////////////////////////////////////////////////////////////////////
+      ////////////////////////////////////////////////////////////////////////// Temas
       theme: ThemeData(
         brightness: Brightness.light,
         primarySwatch: Colors.blue,
