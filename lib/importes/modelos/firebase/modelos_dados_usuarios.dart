5a3f317aed24e457e1137a1e7c0dff54440c61e2
Rauan de Santana Rosa <rauanoronha@hotmail.com>
2023-05-29 05:54:36 -0300

    Adicionando Sistemas Firebase Dados e Modelos Firebase Dados Usuarios

--- lib/importes/modelos/firebase/modelos_dados_usuarios.dart
+++ lib/importes/modelos/firebase/modelos_dados_usuarios.dart
@@ -1,15 +1,56 @@
 import 'package:noronhaecotech/importes/importar_sistemas.dart';
+import 'package:noronhaecotech/importes/sistemas/firebase/firebase_dados.dart';
 
 // ----------------------------------------------------------------------------- Coleção Usuário
-class DadosUsuarios {
-  final colecao = Sistemas.firebase.dados.instancia.collection("usuarios");
+class DadosUsuarios extends Dados {
+  final bool? criarUsuario;
+  // =========================================================================== Usuario Dados Principais
+  final String? uid;
+  final String? imagemPerfil;
+  final String? nomeCompleto;
+  final String? celular;
+  final String? email;
+  final String? senha;
+  // =========================================================================== Dados Secundários
+  final String? nomePublico;
 
-
-  DadosUsuarios();
+  DadosUsuarios({
+    this.criarUsuario,
+    required this.uid,
+    this.imagemPerfil,
+    this.nomeCompleto,
+    this.celular,
+    this.email,
+    this.senha,
+    this.nomePublico,
+  }) : super(
+          criar: criarUsuario,
+          id: uid,
+          colecao: Sistemas.firebase.dados.instancia.collection("usuarios"),
+          objetoSalvar: {
+            // ----------------------------------------------------------------- Usuario Dados Principais
+            if (criarUsuario == true) "uid": uid,
+            if (imagemPerfil != null) "imagemPerfil": imagemPerfil,
+            if (nomeCompleto != null) "nomeCompleto": nomeCompleto,
+            if (celular != null) "celular": celular,
+            if (email != null) "email": email,
+            if (senha != null) "senha": Sistemas.texto.criptografar(senha),
+            // ----------------------------------------------------------------- Dados Secundários
+            if (nomePublico != null) "nomePublico": nomePublico,
+          },
+        );
   //////////////////////////////////////////////////////////////////////////////
 
-
-
-
-
+  @override
+  T converterObjeto<T>(Map<String, dynamic> objeto) {
+    return DadosUsuarios(
+      uid: objeto["uid"],
+      imagemPerfil: objeto["imagemPerfil"],
+      nomeCompleto: objeto["nomeCompleto"],
+      celular: objeto["celular"],
+      email: objeto["email"],
+      senha: objeto["senha"],
+      nomePublico: objeto["nomePublico"],
+    ) as T;
+  }
 }
--- lib/importes/sistemas/firebase/firebase_dados.dart
+++ lib/importes/sistemas/firebase/firebase_dados.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:noronhaecotech/importes/modelos/firebase/modelos_dados_usuarios.dart';
 
 // ----------------------------------------------------------------------------- Sistemas Firebase Dados
 class $SisFirebaseDados {
@@ -8,16 +7,66 @@ class $SisFirebaseDados {
   $SisFirebaseDados();
   //////////////////////////////////////////////////////////////////////////////
 
-  void salvarDados({
-    required CollectionReference colecao,
-    required DadosUsuarios dadosUsuarios,
-}) {
-    final documento = colecao.doc();
-    documento.set(data);
-
-
+  // =========================================================================== Criar Dados
+  Future<bool> salvarDados({required Dados dados}) async {
+    if (dados.id == null && dados.criar != true) return false;
+    final documento = dados.colecao.doc(dados.id);
+    if (dados.criar == true) {
+      return await documento.set(dados.objetoSalvar).then((value) {
+        return true;
+      }).catchError((erro) => false);
+    } else {
+      return await documento.update(dados.objetoSalvar).then((value) {
+        return true;
+      }).catchError((erro) => false);
+    }
   }
 
+  // =========================================================================== Recuperar Dados
+  Future<T?> recuperarDados<T>({required T dadosRecuperar}) async {
+    dadosRecuperar as Dados;
+    final documento = dadosRecuperar.colecao.doc(dadosRecuperar.id);
+    return await documento.get().then((resposta) {
+      if (!resposta.exists) return null;
+      var dados = resposta.data() as Map<String, dynamic>?;
+      if (dados == null) return null;
+      return dadosRecuperar.converterObjeto<T>(dados);
+    }).catchError((erro) => null);
+  }
 
+  // =========================================================================== Observar Alterações
+  Stream<T> observarAlteracoes<T>({required T dadosRecuperar}) {
+    dadosRecuperar as Dados;
+    final documento = dadosRecuperar.colecao.doc(dadosRecuperar.id);
+    return documento.snapshots().map((resposta) {
+      var dados = resposta.data() as Map<String, dynamic>;
+      return dadosRecuperar.converterObjeto<T>(dados);
+    });
+  }
 
+  // =========================================================================== Deletar Dados
+  Future<bool> deletarDados({required Dados dados}) async {
+    if (dados.id == null) return false;
+    final documento = dados.colecao.doc(dados.id);
+    return await documento.delete().then((value) {
+      return true;
+    }).catchError((erro) => false);
+  }
+}
+
+// ----------------------------------------------------------------------------- Tipo Dados
+abstract class Dados {
+  bool? criar;
+  String? id;
+  CollectionReference colecao;
+  Map<String, dynamic> objetoSalvar;
+  T converterObjeto<T>(Map<String, dynamic> objeto);
+
+  Dados({
+    required this.criar,
+    required this.id,
+    required this.colecao,
+    required this.objetoSalvar,
+  });
+  //////////////////////////////////////////////////////////////////////////////
 }
