a339573001f890ff304eadc1d06fce170d990e27
Rauan Rosa <99094020+rauandesantana@users.noreply.github.com>
2023-06-26 00:51:07 -0300

    Correção Generalizada

--- lib/paginas/objetos/rota_inicial.dart
+++ lib/paginas/objetos/rota_inicial.dart
@@ -26,7 +26,7 @@ class _RotaInicialState extends State<RotaInicial> {
                     fecharTodas: true,
                     pagina: Paginas.acesso.login,
                   ),
-                  titulo: "entrar",
+                  titulo: "entrar...",
                 ),
               ],
             ),
