82bf651b11d9d8cfd93280a1fcb73c1d677c04b7
Rauan Rosa <99094020+rauandesantana@users.noreply.github.com>
2023-06-27 10:13:19 -0300

    Correção Generalizada

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,19 @@ void main() {
   usePathUrlStrategy();
 
 
+  runApp(const MaterialApp(
+    home: Teste(),
+  ),);
 
 
-
-  runApp(Paginas.carregamento);
+  // runApp(Paginas.carregamento);
   WidgetsFlutterBinding.ensureInitialized();
   Future.wait([
     //////////////////////////////////////////////////////////////////////////// Carregar Dependencias
     Idiomas.load(Idiomas.delegate.supportedLocales.first),
     Sistemas.firebase.inicializar,
     ////////////////////////////////////////////////////////////////////////////
+    Future.delayed(const Duration(seconds: 200)),
     Future.delayed(const Duration(seconds: 2)),
   ]).whenComplete(() {
     //////////////////////////////////////////////////////////////////////////// Configuração
@@ -34,4 +37,21 @@ void main() {
     ////////////////////////////////////////////////////////////////////////////
     runApp(config.materialApp);
   });
+}
+
+class Teste extends StatelessWidget {
+  const Teste({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          TextFormField(
+
+          )
+        ],
+      ),
+    );
+  }
 }
